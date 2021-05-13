d2133ded33c6cb3ac9a6e55b0b85ad2a2bc0790e
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 10:34:49 +0530

    Created the GameViewModel instance in GameFragment.kt using kotlin property delegate.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
 
@@ -29,6 +30,7 @@ import com.example.android.unscramble.databinding.GameFragmentBinding
  */
 class GameFragment : Fragment() {
 
+    private val viewModel : GameViewModel by viewModels()
     private var score = 0
     private var currentWordCount = 0
     private var currentScrambledWord = "test"
