a9fd89439e54a6d65f3642e07e5c7383fe557c09
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 10:40:27 +0530

    Copied all 3 variables (score, currentWordCount and currentScrambleWord) to GameViewModel.kt class.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,4 +3,7 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
+    private var score = 0
+    private var currentWordCount = 0
+    private var currentScrambledWord = "test"
 }
