1565f1585e0d1c82d262095324c8e2247ca63843
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 11:18:52 +0530

    Created the backing property for _score.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,7 +3,9 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
-    private var score = 0
+    private var _score = 0
+    val score : Int get() = _score
+
     private var _currentWordCount = 0
     val currentWordCount : Int get() = _currentWordCount
 
