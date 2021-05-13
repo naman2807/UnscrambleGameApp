c013240a2b71f3a181e5e1dd5fa2ad4b33c28007
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 10:44:21 +0530

    Created the private variable and its immutable field which can be read only by other classes using backing property.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,4 +6,7 @@ class GameViewModel : ViewModel(){
     private var score = 0
     private var currentWordCount = 0
     private var currentScrambledWord = "test"
+
+    private var _count = 0
+    val count : Int get() = _count
 }
